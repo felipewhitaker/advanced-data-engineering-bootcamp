337e97d31456fe9979731806a64fdf75efa4eb4f
Felipe Whitaker <felipewhitaker@gmail.com>
2022-01-29 20:18:02 -0300

    Add Kinesis to `app`

--- app.py
+++ app.py
@@ -10,21 +10,10 @@ from aws_cdk import core as cdk
 from aws_cdk import core
 
 from data_lake.stack import DataLakeStack
+from kinesis.stack import KinesisStack
 
 app = core.App()
-DataLakeStack(
-    app,
-    # "DataLakeStack",
-    # If you don't specify 'env', this stack will be environment-agnostic.
-    # Account/Region-dependent features and context lookups will not work,
-    # but a single synthesized template can be deployed anywhere.
-    # Uncomment the next line to specialize this stack for the AWS Account
-    # and Region that are implied by the current CLI configuration.
-    # env=core.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')),
-    # Uncomment the next line if you know exactly what Account and Region you
-    # want to deploy the stack to. */
-    # env=core.Environment(account='123456789012', region='us-east-1'),
-    # For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
-)
+data_lake_stack = DataLakeStack(app)
+kinesis_stack = KinesisStack(app, data_lake_raw_bucket=data_lake_stack.data_lake_raw_bucket)
 
 app.synth()
